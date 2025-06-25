a8ec0010fffb11b7e72543070a0d552b1fbd8a67
chandanv1010 <tuannc.dev@gmail.com>
2025-06-25 18:05:57 +0700

    update Google Analytics

--- resources/views/frontend/homepage/layout.blade.php
+++ resources/views/frontend/homepage/layout.blade.php
@@ -1,7 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
+        <!-- Google tag (gtag.js) -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-9XNKRDPL8T"></script>
+        <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-9XNKRDPL8T');
+        </script>
         @include('frontend.component.head')
+        
     </head>
     @if(isset($schema))
         {!! $schema !!}
--- resources/views/mobile/homepage/layout.blade.php
+++ resources/views/mobile/homepage/layout.blade.php
@@ -1,6 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
+        <!-- Google tag (gtag.js) -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-9XNKRDPL8T"></script>
+        <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-9XNKRDPL8T');
+        </script>
         @include('mobile.component.head')
     </head>
     <body>
